59a3f818cef5e216f0e6d0e6202a66411b2fbd37
ohuton55 <tttt.tttt.no@gmail.com>
2022-11-02 22:28:53 +0900

    checking in 'getFullYear,getMonth,getDate'

--- renderer.js
+++ renderer.js
@@ -1,20 +1,2 @@
-const nowDate = new Date();
-let year = nowDate.getFullYear();
-let month = nowDate.getMonth() + 1;
-let date = nowDate.getDate();
-let text = year + "/" + month + "/" + date;
-const yymmdd = document.getElementById('yymmdd');
-yymmdd.innerText = text;
-
-const title = document.getElementById('title');
-title.innerText = 'Happy Helloween!';
-
 const information = document.getElementById('info');
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
-
-document.write(AA);
-
-const func = async () => {
-        const response = await window.versions.ping();
-        consle.log(response);
-}
